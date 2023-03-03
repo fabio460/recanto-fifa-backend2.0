daf557794b41be36ae056df6f52e86d5df8d1f35
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-03 20:41:26 -0300

    teste 7

--- index.ts
+++ index.ts
@@ -4,11 +4,11 @@ import cors from 'cors'
 import route from './src/rotas'
 app.use(express.json())
 app.use(cors())
-route.forEach(elem=>{
-   return app.use(elem.endpoint,elem.rota)
-})
-// app.get("/",(req,res)=>{
-//     res.send("ola mundo")
+// route.forEach(elem=>{
+//    return app.use(elem.endpoint,elem.rota)
 // })
+app.get("/",(req,res)=>{
+    res.send("ola mundo")
+})
 
 app.listen(4000,()=>console.log("escutando na porta 4000"))
