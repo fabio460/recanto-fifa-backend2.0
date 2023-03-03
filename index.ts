7feb90b5ba2d8e0a3c18241591b517a73a8ad877
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-03 20:20:56 -0300

    teste 3

--- index.ts
+++ index.ts
@@ -4,8 +4,10 @@ import cors from 'cors'
 import route from './src/rotas'
 app.use(express.json())
 app.use(cors())
-route.forEach(elem=>{
-   return app.use(elem.endpoint,elem.rota)
+// route.forEach(elem=>{
+//    return app.use(elem.endpoint,elem.rota)
+// })
+app.get("/",(req,res)=>{
+    res.send("ola mundo")
 })
-
 app.listen(4000,()=>console.log("escutando na porta 4000"))
