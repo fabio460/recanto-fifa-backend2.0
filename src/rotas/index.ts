71e9c36e106d018d9f1492043e89abd699f141ca
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-03 20:49:14 -0300

    teste 9

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -4,8 +4,8 @@ import jogadorRouter from "./jogadorRouter";
 import usuarioRouter from "./usuarioRouter";
 
 const route:rotaType[] = [
-    {endpoint:"/usuario",rota:usuarioRouter},
-    {endpoint:"/jogador",rota:jogadorRouter},
+    // {endpoint:"/usuario",rota:usuarioRouter},
+    // {endpoint:"/jogador",rota:jogadorRouter},
     {endpoint:"/",rota:defaultRouter},
 ]
 
